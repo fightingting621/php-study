6d5dd06e9f60b5af8c91e6e6131865e57ba71521
Dell <qwertxz>
2018-10-14 11:33:42 +0800

    php $_POST

--- login.php
+++ login.php
@@ -1,3 +1,21 @@
+<?php
+    $flag=false;
+    if ($_SERVER['REQUEST_METHOD']=='POST'){
+        $name=$_POST['username'];
+        $p=$_POST['pwd'];
+        if ($name=='admin'&&$p=='1111'){
+            $flag=true;
+            echo "登录成功";
+        }else{
+            $flag=false;
+        }
+    }else {
+
+    }
+?>
+
+
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -8,10 +26,24 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="server.php">
+
+    <?php
+        if($flag==false){
+    ?>
+    <form action="login.php" method="post">
         用户名： <input type="text" name="username"><br>
         密码： <input type="password" name="pwd"><br>
         <input type="submit" value="登录">
     </form>
+            <?php
+            if ($_SERVER['REQUEST_METHOD']=='POST') {
+
+                    echo "用户名或密码错误";
+                }
+
+            ?>
+    <?php
+        }
+    ?>
 </body>
 </html>
--- server.php
+++ server.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Dell
- * Date: 2018/10/14
- * Time: 9:29
- */
-$name=$_GET['username'];
-$p=$_GET['pwd'];
+
+$name=$_POST['username'];
+$p=$_POST['pwd'];
 echo "您的用户名是：".$name,"您的密码是：".$p;
